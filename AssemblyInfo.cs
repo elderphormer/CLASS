9f75cbf94e8a795e5b44f0a9f8c8a1d359de0f4d
Lao Chung <goncalopcbg1@gmail.com>
2026-05-05 09:12:48 +0100

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nums240083
+{
+    public class Numeros
+    {
+        private Num33Zero3[] m_numeros;
+        private int m_qtdNums;
+
+        public Numeros(int capacidade)
+        {
+            m_numeros = new Num33Zero3[capacidade];
+            m_qtdNums = 0;
+        }
+
+        public int qtdNums
+        {
+            get { return m_qtdNums; }
+            set { m_qtdNums = value; }
+    }
+}
+
---
+++ Num33Zero3.cs
@@ -0,0 +1,36 @@
+{
+  "Version": 1,
+  "WorkspaceRootPath": "C:\\Users\\I240083\\source\\repos\\Nums240083\\",
+  "Documents": [
+    {
+      "AbsoluteMoniker": "D:0:0:{3B7A4766-D38A-4EE5-A3C7-48DCA34542F6}|Nums240083\\Nums240083.csproj|c:\\users\\i240083\\source\\repos\\nums240083\\nums240083\\form1.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}|Form",
+      "RelativeMoniker": "D:0:0:{3B7A4766-D38A-4EE5-A3C7-48DCA34542F6}|Nums240083\\Nums240083.csproj|solutionrelative:nums240083\\form1.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}|Form"
+    }
+  ],
+  "DocumentGroupContainers": [
+    {
+      "Orientation": 0,
+      "VerticalTabListWidth": 256,
+      "DocumentGroups": [
+        {
+          "DockedWidth": 200,
+          "SelectedChildIndex": 0,
+          "Children": [
+            {
+              "$type": "Document",
+              "DocumentIndex": 0,
+              "Title": "Form1.cs [Design]",
+              "DocumentMoniker": "C:\\Users\\I240083\\source\\repos\\Nums240083\\Nums240083\\Form1.cs",
+              "RelativeDocumentMoniker": "Nums240083\\Form1.cs",
+              "ToolTip": "C:\\Users\\I240083\\source\\repos\\Nums240083\\Nums240083\\Form1.cs [Design]",
+              "RelativeToolTip": "Nums240083\\Form1.cs [Design]",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-04-29T07:39:06.344Z",
+              "EditorCaption": " [Design]"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
